5048e45457fd1f19655730518073f04a36773859
fariafarzana11 <fariamukta00@gmail.com>
2024-09-29 10:16:23 +0600

    updated

--- reverse.js
+++ reverse.js
@@ -1,6 +1,7 @@
 let original = "Hello World";
-// let reversed = [...original].reverse().join('');
-// console.log(reversed);
+let reversed = original.split("").reverse().join('');
+console.log(reversed);
+//console.log([original].reverse().join())
 
 
 // let original = "Hello World";
@@ -12,10 +13,33 @@ let original = "Hello World";
 
 
 //1ta kore letter reverse hobe + ager letter ta asthe thakbe
+// let sentence = "Hello World";
+// let reverse = "";
+// for(let i = sentence.length - 1; i >=0; i--){
+//     reverse += sentence[i];
+//     console.log(reverse)
+// }
+
+
 
 let sentence = "Hello World";
 let reverse = "";
-for(let i = sentence.length - 1; i >=0; i--){
-    reverse += sentence[i];
-    console.log(reverse)
+for(let i = 0;i<sentence.length;i++){
+    const newValue = sentence[i];
+    reverse = newValue + reverse; //dlroW olleH
+    // reverse = reverse + newValue; //hello world
 }
+console.log(reverse)
+
+
+// let sentence = "Hello World";
+// let reverse = ''
+// for(const rev of sentence){
+//     //console.log(rev)
+//     reverse = rev + reverse;
+// }
+// console.log(reverse)
+
+
+// const arr = [2,7,9,11,67,87,99];
+// console.log(arr.reverse())
